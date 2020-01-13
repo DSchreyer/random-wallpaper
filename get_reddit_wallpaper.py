a1b753e8e5f1c89678d1adcef9ca827f4f868b3e
DSchreyer <daniel.schreyer@fau.de>
2020-01-13 16:10:17 +0100

    Fixed coding style with pylint

--- get_reddit_wallpaper.py
+++ get_reddit_wallpaper.py
@@ -1,30 +1,33 @@
 #!/usr/bin/env python
+"""
+This is a program that downloads the top image from a reddit subreddit and stores it in a directory.
+"""
 # coding: utf-8
 
-from bs4 import BeautifulSoup
-import requests
-import re
-import os
 import argparse
-from sys import platform
 import ctypes
-from appscript import app, mactypes
+import os
+from sys import platform
+import re
+import requests
+from bs4 import BeautifulSoup
+# from appscript import app, mactypes
 
 
 # Parse arguments into script
 parser = argparse.ArgumentParser(description="Get Wallpaper from subreddit wallpaper.")
-parser.add_argument( "--category",
-                     type=str,
-                     default = "top",
-                     help = "Category: [hot | top | new | controversial | rising]")
-parser.add_argument( "--directory",
-                     help = "Path to directory in which images should be stored")
-parser.add_argument( "--change_wallpaper",
-                     default= "True",
-                     help = "Change Wallpaper? ['True' | 'False']")
-parser.add_argument( "--subreddit",
-                     default= "wallpapers",
-                     help = "Subreddit to download top image from")
+parser.add_argument("--category",
+                    type=str,
+                    default="top",
+                    help="Category: [hot | top | new | controversial | rising]")
+parser.add_argument("--directory",
+                    help="Path to directory in which images should be stored")
+parser.add_argument("--change_wallpaper",
+                    default="True",
+                    help="Change Wallpaper? ['True' | 'False']")
+parser.add_argument("--subreddit",
+                    default="wallpapers",
+                    help="Subreddit to download top image from")
 args = parser.parse_args()
 subreddit = "https://www.reddit.com/r/%s" % (args.subreddit)
 
@@ -35,16 +38,32 @@ if args.directory is not None:
 else:
     directory = os.popen("pwd").read().strip()
 
-class get_wallpaper():
+class GetWallpaper():
+    """
+    Downloads and stores wallpaper image from a reddit subreddit.
+    """
     def __init__(self, url):
         self.url = url
+        self.href = ""
+        self.src = ""
+        self.title = ""
+        self.img_href = ""
+        self.path = ""
 
     def get_href(self, page_link):
+        """
+        Download html content from reddit.
+
+        +
+
+        Identifies first image on subreddit.
+        """
         # get html from website
         page_response = requests.get(page_link, headers=headers)
         page_content = BeautifulSoup(page_response.text, "html.parser")
 
-        posts = page_content.find("div", {"class": "_3JgI-GOrkmyIeDeyzXdyUD _2CSlKHjH7lsjx0IpjORx14"})
+        posts = page_content.find("div",
+                                  {"class": "_3JgI-GOrkmyIeDeyzXdyUD _2CSlKHjH7lsjx0IpjORx14"})
         img = posts.find("img")
         href = posts.find("a").attrs["href"]
 
@@ -54,15 +73,24 @@ class get_wallpaper():
         self.src = src
 
     def get_image(self, href):
+        """
+        Get title name of image and remove special characters
+        """
         next_page = requests.get(href, headers=headers)
         next_content = BeautifulSoup(next_page.text, "html.parser")
         # find image url
         img_a = next_content.find("div", {"class": "media-preview-content"}).find("a")
         self.img_href = img_a.attrs["href"]
         title = next_content.find("a", {"class": "title"}).text
-        self.title = title.replace("/", "_").replace("?", "_").replace('"',"_").replace("*", "_").replace("\\","_").replace("<", "_").replace(">", "_").replace("|", "_").replace(":","_").replace("'","")
+        title = title.replace("/", "_").replace("?", "_").replace('"', "_")
+        title = title.replace("*", "_").replace("\\", "_").replace("<", "_")
+        title = title.replace(">", "_").replace("|", "_").replace(":", "_").replace("'", "")
+        self.title = title
     def download_image(self, title, img_href, pwd):
-        format_img = re.search("^.*(\..*)$", img_href).group(1)
+        """
+        Download image and save it in specified directory
+        """
+        format_img = re.search("^.*(\..+)$", img_href).group(1)
         title = (title + format_img)
         self.path = ("%s/%s" % (pwd, title))
         print("Downloaded %s and saved it in %s" % (title, self.path))
@@ -70,7 +98,7 @@ class get_wallpaper():
 for cat in args.category.strip().split(","):
     link = "%s/%s/" % (subreddit, cat)
     print("Download image from: %s" % (link))
-    post = get_wallpaper(link)
+    post = GetWallpaper(link)
 
     post.get_href(link)
     post.get_image(post.href)
@@ -82,20 +110,20 @@ for cat in args.category.strip().split(","):
 
     # download image
     if not os.path.exists(post.path):
-        f = open(post.path,'wb')
+        f = open(post.path, 'wb')
         f.write(requests.get(post.img_href).content)
         f.close()
 
     # change wallpaper to downloaded image
-    if platform == "linux" or platform == "linux2":
+    if platform in ("linux", "linux2"):
         if args.change_wallpaper == "True":
             os.system("gsettings set org.gnome.desktop.background picture-uri '%s'" % (full_name))
             print("Change wallpaper in Unix os")
             print("Changed wallpaper to: %s" % (post.path))
-    elif platform == "darwin":
-        print("Change wallpaper in OS X")
-        app('Finder').desktop_picture.set(mactypes.File(post.path))
-        print("Changed wallpaper to: %s" % (post.path))
+    # elif platform == "darwin":
+    #    print("Change wallpaper in OS X")
+    #    app('Finder').desktop_picture.set(mactypes.File(post.path))
+    #    print("Changed wallpaper to: %s" % (post.path))
     elif platform == "win32":
         print("Change wallpaper in Windows")
         normpath = os.path.normpath(post.path)
@@ -105,5 +133,3 @@ for cat in args.category.strip().split(","):
     else:
         print("Operating system is not recognized")
         print("Does not change the wallpaper")
-
-
